73740d88cbaeb962a15439cf65c0a3b1e0ce5708
villordojack0-rgb <villordojack0@gmail.com>
2025-10-23 22:19:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("factor-a-v1").then(cache => {
+    caches.open("factor-a-v2").then(cache => {
       return cache.addAll([
         "index.html",
         "styles.css",
