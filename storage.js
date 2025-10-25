1e45ef7594c9f46770521e4f5e63afe0b77fc347
villordojack0-rgb <villordojack0@gmail.com>
2025-10-24 21:34:58 -0300

    Delete storage.js

--- storage.js
+++
@@ -1,9 +0,0 @@
-// --- MANEJO DE LOCALSTORAGE ---
-export function guardarDatos(clave, datos) {
-  localStorage.setItem(clave, JSON.stringify(datos));
-}
-
-export function obtenerDatos(clave) {
-  const datos = localStorage.getItem(clave);
-  return datos ? JSON.parse(datos) : [];
-}
