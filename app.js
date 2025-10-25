12b76e6d6da8fa80e7735618b54545e1a5f0c560
villordojack0-rgb <villordojack0@gmail.com>
2025-10-24 21:35:24 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// PWA Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log("Service Worker registrado."))
-      .catch(err => console.log("Error al registrar SW:", err));
-  });
-}
